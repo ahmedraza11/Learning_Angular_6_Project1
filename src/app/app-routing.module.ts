4372ad90a3ea65dbcc87fb48f0601fda752a41a9
Ahmed Raza Qadri <ahmedrazaqadri@pop-os.localdomain>
2019-03-18 11:11:58 +0500

    Project init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { PostComponent } from './post/post.component';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: UsersComponent
+  },
+  {
+    path: "posts",
+    component: PostComponent
+  },
+  {
+    path: "details/:id",
+    component: DetailComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { UsersComponent } from './users/users.component';
+import { PostComponent } from './post/post.component';
+import { DetailComponent } from './detail/detail.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    UsersComponent,
+    PostComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
